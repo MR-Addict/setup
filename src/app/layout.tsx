daf6e62289baaf43a429842b3151b2f71c3221bd
MR-Addict <2750417853@qq.com>
2023-07-29 12:35:10 +0800

    restore: remove umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 
-import Script from "next/script";
 import { Navbar, Footer } from "@/components";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -11,7 +10,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         <Footer />
       </body>
-      <Script async src="https://umami.mraddict.one/script.js" data-website-id="94d844bb-644b-43c9-887e-ce5933fc28ab" />
     </html>
   );
 }
